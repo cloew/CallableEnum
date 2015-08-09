b5857d5a27c0faf7c17857831cf757c0667b7e97
Chris Loew <cloew123@gmail.com>
2015-08-09 12:35:40 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='callable_enum',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['callable_enum'],
+     )
